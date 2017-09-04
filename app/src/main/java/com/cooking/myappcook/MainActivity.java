c81c22ab94f934ef965ad0241105254bc96419d7
y.kosmina <wpanterka@gmail.com>
2017-09-04 16:47:25 +0300

    Lesson 5_ 8 done

--- app/src/main/java/com/cooking/myappcook/MainActivity.java
+++ app/src/main/java/com/cooking/myappcook/MainActivity.java
@@ -6,6 +6,8 @@ import android.view.View;
 import android.widget.TextView;
 import java.text.NumberFormat;
 
+import static android.R.attr.id;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,26 +19,49 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void submitOrder(View view) {
-        int numberOfCoffees = 2;
-        display(numberOfCoffees);
-        displayPrice(numberOfCoffees * 5);
+    /** this method is called when the button "Order" is clicked */
+
+     public void submitOrder(View view) {
+        int price = calculatePrice();
+        String priceMessage = "That would be $" + price + "\nThanks.";
+        displayMessage(priceMessage);
+
     }
 
+    /**
+     * Calculates the price of the order.
+     *@return total price
+     */
+    private int calculatePrice() {
+        int price = quantity * 5;
+        return price;
+    }
+
+
+
+    /**
+     * This method displays the given text on the screen.
+     */
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
+
+    }
     public void increment(View view) {
         quantity = quantity + 1;
-        display(quantity);
+        displayQuantity(quantity);
     }
 
     public void decrement (View view) {
         quantity = quantity - 1;
-        display(quantity);
+        displayQuantity(quantity);
     }
 
-    private void display(int number) {
+    private void displayQuantity(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
     }
+    /** This method display the given quantity value on the screen */
 
     private void displayPrice(int number) {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
